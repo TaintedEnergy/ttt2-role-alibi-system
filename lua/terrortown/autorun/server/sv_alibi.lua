445345952b3ae528f6ed0e4ef1abd577380a3240
TaintedEnergy <taintedenergy@gmail.com>
2021-11-10 18:45:25 +1300

    alibi on role change

--- lua/terrortown/autorun/server/sv_alibi.lua
+++ lua/terrortown/autorun/server/sv_alibi.lua
@@ -1,53 +1,50 @@
 AddCSLuaFile("cl_alibi_init.lua")
 util.AddNetworkString("ttt2SendRoleAlibi")
 
-hook.Add("TTTBeginRound", "TTT2RequestRoleAlibiBeginRound", function()
-    timer.Create("alibi", 1, 1, function()
+hook.Add("TTT2UpdateSubrole", "TTT2CreateAlibi", function(ply, oldSubrole, newSubrole)
+    timer.Simple(0.1, function() 
+        if (IsValid(ply) and ply:GetSubRoleData().defaultTeam ~= "innocents") then
+            local selectablePlys = roleselection.GetSelectablePlayers(player.GetAll())
+            local alibiRoleCandidates = table.Copy(roleselection.GetAllSelectableRolesList(#selectablePlys))
+            local avoidRoles = {"detective", "banker", "revolutionary", "sheriff", "sniffer", "vigilante", "wrath", "shinigami", "liar", "bodyguard", "cupid", "ghost"}
+            local alibiRoles = {}
+            local alibiRolesCount = 0
+            
+            if alibiRoleCandidates == nil then return end
 
-        local selectablePlys = roleselection.GetSelectablePlayers(player.GetAll())
-        local alibiRoleCandidates = table.Copy(roleselection.GetAllSelectableRolesList(#selectablePlys))
-        local alibiRoles = {}
-        local count = 0
-        local avoidRoles = {"detective", "banker", "revolutionary", "sheriff", "sniffer", "vigilante", "wrath", "shinigami", "liar", "bodyguard", "cupid"}
-        
-        if alibiRoleCandidates == nil then return end
-
-        for k in pairs(alibiRoleCandidates) do
-            local tempRole = roles.GetByIndex(k) 
-            if tempRole.defaultTeam == "innocents" then
-                local allow = true
-                for j in pairs(avoidRoles) do
-                    if tempRole.name == avoidRoles[j] then
-                        allow = false
-                        break
+            for k in pairs(alibiRoleCandidates) do
+                local tempRole = roles.GetByIndex(k) 
+                if tempRole.defaultTeam == "innocents" then
+                    local allow = true
+                    for j in pairs(avoidRoles) do
+                        if tempRole.name == avoidRoles[j] then
+                            allow = false
+                            break
+                        end
+                    end
+                    if allow then
+                        table.insert(alibiRoles, tempRole.name)
+                        alibiRolesCount = alibiRolesCount + 1
                     end
                 end
-                if allow then
-                    table.insert(alibiRoles, tempRole.name)
-                    count = count + 1
-                end
             end
-        end
 
-        net.Start("ttt2SendRoleAlibi")
-        for _,ply in ipairs(player:GetAll()) do
-            if (IsValid(ply) and ply:GetSubRoleData().defaultTeam ~= "innocents") then
-                local alibiName = alibiRoles[math.random(1, count)]
-                local plyRoleName = ply:GetSubRoleData().name
+            local alibiName = alibiRoles[math.random(1, alibiRolesCount)]
+            local plyRoleName = ply:GetSubRoleData().name
 
-                if plyRoleName == "defective" then
-                    alibiName = "detective"
-                elseif plyRoleName == "jester" or plyRoleName == "swapper" or plyRoleName == "clown" then
-                    alibiName = "beggar"
-                elseif plyRoleName == "doppelganger" then
-                    alibiName = "mimic"
-                elseif plyRoleName == "beggar" or plyRoleName == "medic" or plyRoleName == "bodyguard" or plyRoleName == "unknown" or plyRoleName == "pirate_captain" or plyRoleName == "none" then
-                    break
-                end
-
-                net.WriteString(alibiName)
-                net.Send(ply)
+            if plyRoleName == "defective" then
+                alibiName = "detective"
+            elseif plyRoleName == "jester" or plyRoleName == "swapper" or plyRoleName == "clown" then
+                alibiName = "beggar"
+            elseif plyRoleName == "doppelganger" then
+                alibiName = "mimic"
+            elseif plyRoleName == "beggar" or plyRoleName == "medic" or plyRoleName == "bodyguard" or plyRoleName == "unknown" or plyRoleName == "pirate_captain" or plyRoleName == "none" then
+                return
             end
+
+            net.Start("ttt2SendRoleAlibi")
+            net.WriteString(alibiName)
+            net.Send(ply)
         end
     end)
 end)
